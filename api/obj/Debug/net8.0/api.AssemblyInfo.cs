495a3b5ed40db3c2222185d3028dd949349a402c
jonathanrochoa <ochoajonathan@icloud.com>
2024-08-28 20:58:38 -0400

    Changes to Main folder

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96eb25a65dd89a8bed377b8cc49eeb3e0b7b1005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f4589a476a1ad37468bcb2b2c574d08450c981")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
