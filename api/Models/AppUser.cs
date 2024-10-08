5272aff84a54a02b89764fe0d24235d6177e7d8f
jonathanrochoa <ochoajonathan@icloud.com>
2024-10-07 21:42:24 -0400

    Implemented JWT Identity

---
+++ api/Models/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+//additions 10/7
+namespace api.Models
+{
+    public class AppUser : IdentityUser
+    {
+        
+    }
+}
